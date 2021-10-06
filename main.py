1bec1b4866078a30963627716b777644600c26ec
Rivixer <76163846+Rivixer@users.noreply.github.com>
2021-10-06 12:48:38 +0200

    Działa, ale jest bardzo nieczytelne. Nie jestem zadowolny

--- main.py
+++ main.py
@@ -1,55 +1,114 @@
-from typing import List
-
 class PrzeprawaPrzezRzeczke:
+    uczestnicy = {'a': 1, 'b': 4, 'c': 5, 'd': 10}
+    przed = []
+    po = []
 
-    przed: List[int] = list()
-    po: List[int] = list()
-    liczba_uczestnikow: int = None
-    
-    def wczytaj_dane(self):
-        #self.uczestnicy = [int(i) for i in input('Podaj minuty uczestników, oddzielając ich przecinkiem: ').split(',')]
-        self.przed = [1, 2, 5, 10]
-        self.liczba_uczestnikow = len(self.przed)
-
-    def _wybierz_przed(self):
-        for os1 in self.przed:
-            for os2 in self.przed:
-                if os1 == os2:
+    def wszystkie_mozliwosci_przed(self, przed: list):
+        to_return = []
+        for u1 in przed:
+            for u2 in przed:
+                if u1 == u2 or sorted([u1, u2]) in to_return:
                     continue
-                yield os1, os2
+                to_return.append(sorted([u1, u2]))
+        return to_return
 
-    def przeprowadz_przez_most(self, os1, os2):
-        self.przed.remove(os1)
-        self.przed.remove(os2)
-        self.po.append(os1)
-        self.po.append(os2)
+    def wszystkie_mozliwosci_po(self, po: list):
+        to_return = []
+        for u in po:
+            to_return.append(u)
+        return to_return
 
-    def _wroc_z_powrotem(self, os):
-        self.po.remove(os)
-        self.przed.append(os)
 
-    def _wybierz_po(self):
-        for os in self.po:
-            yield os
+    def start(self):
+        self.przed_kopia0 = list(self.uczestnicy.keys())
+        self.po_kopia0 = []
+        self.czas_kopia0 = 0
+        i = 0
+        wyniki = []
+        for i0 in self.wszystkie_mozliwosci_przed(self.przed_kopia0):
+            self.przed = self.przed_kopia0.copy()
+            self.po = self.po_kopia0.copy()
+            self.czas = self.czas_kopia0 + max(self.uczestnicy[i0[0]], self.uczestnicy[i0[1]])
+            self.przed.remove(i0[0])
+            self.przed.remove(i0[1])
+            self.po.append(i0[0])
+            self.po.append(i0[1])
+            self.przed_kopia1 = self.przed.copy()
+            self.po_kopia1 = self.po.copy()
+            self.czas_kopia1 = self.czas
+            for i1 in self.wszystkie_mozliwosci_po(self.po_kopia1):
+                self.przed = self.przed_kopia1.copy()
+                self.po = self.po_kopia1.copy()
+                self.czas = self.czas_kopia1 + self.uczestnicy[i1]
+                self.po.remove(i1)
+                self.przed.append(i1)
+                self.przed_kopia2 = self.przed.copy()
+                self.po_kopia2 = self.po.copy()
+                self.czas_kopia2 = self.czas
+                for i2 in self.wszystkie_mozliwosci_przed(self.przed_kopia2):
+                    self.przed = self.przed_kopia2.copy()
+                    self.po = self.po_kopia2.copy()
+                    self.czas = self.czas_kopia2 + max(self.uczestnicy[i2[0]], self.uczestnicy[i2[1]])
+                    self.przed.remove(i2[0])
+                    self.przed.remove(i2[1])
+                    self.po.append(i2[0])
+                    self.po.append(i2[1])
+                    self.przed_kopia3 = self.przed.copy()
+                    self.po_kopia3 = self.po.copy()
+                    self.czas_kopia3 = self.czas
+                    for i3 in self.wszystkie_mozliwosci_po(self.po_kopia3):
+                        self.przed = self.przed_kopia3.copy()
+                        self.po = self.po_kopia3.copy()
+                        self.czas = self.czas_kopia3 + self.uczestnicy[i3]
+                        self.po.remove(i3)
+                        self.przed.append(i3)
+                        self.przed_kopia4 = self.przed.copy()
+                        self.po_kopia4 = self.po.copy()
+                        self.czas_kopia4 = self.czas
+                        for i4 in self.wszystkie_mozliwosci_przed(self.przed_kopia4):
+                            self.przed = self.przed_kopia4.copy()
+                            self.po = self.po_kopia4.copy()
+                            self.czas = self.czas_kopia4 + max(self.uczestnicy[i4[0]], self.uczestnicy[i4[1]])
+                            self.przed.remove(i4[0])
+                            self.przed.remove(i4[1])
+                            self.po.append(i4[0])
+                            self.po.append(i4[1])
+                            i += 1
+                            wyniki.append([i0, i1, i2, i3, i4, self.czas])
 
-    def sprawdz(self):
-        while True:
-            for os1 in self.przed:
-                for os2 in self.przed:
-                    if os1==os2:
-                        continue
-                    self.przeprowadz_przez_most(os1, os2)
-                    if len(self.po) == self.liczba_uczestnikow:
-                        break
-                    for os in self._wybierz_po():
-                        self._wroc_z_powrotem(os)
-                    
-                
+        min_czas = min(map(lambda i: i[5], wyniki))
+        for wynik in wyniki:
+            przed = list(self.uczestnicy.keys())
+            po = []
+            if wynik[5] == min_czas:
+                print('------------------')
+                print(f'CZAS: {min_czas}')
+                print(f"{', '.join(przed)} \t| {' '.join(po)}")
+                przed.remove(wynik[0][0])
+                przed.remove(wynik[0][1])
+                po.append(wynik[0][0])
+                po.append(wynik[0][1])
+                print(f"{', '.join(sorted(przed))} \t\t| {' '.join(sorted(po))}")
+                po.remove(wynik[1])
+                przed.append(wynik[1])
+                print(f"{', '.join(sorted(przed))} \t| {' '.join(sorted(po))}")
+                przed.remove(wynik[2][0])
+                przed.remove(wynik[2][1])
+                po.append(wynik[2][0])
+                po.append(wynik[2][1])
+                print(f"{', '.join(sorted(przed))} \t\t| {' '.join(sorted(po))}")
+                po.remove(wynik[3])
+                przed.append(wynik[3])
+                print(f"{', '.join(sorted(przed))} \t\t| {' '.join(sorted(po))}")
+                przed.remove(wynik[4][0])
+                przed.remove(wynik[4][1])
+                po.append(wynik[4][0])
+                po.append(wynik[4][1])
+                print(f"{', '.join(sorted(przed))} \t\t| {' '.join(sorted(po))}")
+                print('------------------')
 
-    def pokaz_dane(self):
-        print(self.uczestnicy)
 
+        
 
 zadanie = PrzeprawaPrzezRzeczke()
-zadanie.wczytaj_dane()
-zadanie.sprawdz()
+zadanie.start()
