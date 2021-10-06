b655cc422b8af311abd25a07a391d5863736f57f
Rivixer <76163846+Rivixer@users.noreply.github.com>
2021-10-06 20:51:58 +0200

    Add recursive and end the project

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
-class PrzeprawaPrzezRzeczke:
-    uczestnicy = {'a': 1, 'b': 4, 'c': 5, 'd': 10}
-    przed = []
-    po = []
+import time
 
-    def wszystkie_mozliwosci_przed(self, przed: list):
+class PrzeprawaPrzezMost:
+    uczestnicy = {}
+    instrukcje = []
+
+    def wczytaj_dane(self):
+        for i, czas_uczestnika in enumerate(input('Podaj czasy uczestników (w minutach), oddzielając je przecinkiem: ').split(',')):
+            try:
+                self.uczestnicy[chr(i+97)] = int(czas_uczestnika)
+            except ValueError:
+                print(f'Podałeś/aś złą daną ({czas_uczestnika}), spróbuj ponownie!')
+                self.uczestnicy = {}
+                return self.wczytaj_dane()
+
+    def _dodaj_do_instrukcji(self, instrukcja: list, przed: list, po: list, czas: int):
+        instrukcja.append(f'{" ".join(sorted(przed)) + "  "*len(po)}{" " if len(przed) else ""}| {" ".join(sorted(po)) + "  "*len(przed)} ({czas})')
+
+    def _wszystkie_mozliwosci_przed(self, przed: list):
         to_return = []
         for u1 in przed:
             for u2 in przed:
@@ -12,103 +25,65 @@ class PrzeprawaPrzezRzeczke:
                 to_return.append(sorted([u1, u2]))
         return to_return
 
-    def wszystkie_mozliwosci_po(self, po: list):
-        to_return = []
-        for u in po:
-            to_return.append(u)
-        return to_return
+    def _przesun_do_po(self, przed_kopia: list, po_kopia: list = list(), czas_kopia: int = 0, instrukcja_kopia: list = None):
+        for uczestnik1, uczestnik2 in self._wszystkie_mozliwosci_przed(przed_kopia):
+            po = po_kopia.copy()
+            przed = przed_kopia.copy()
+            czas = czas_kopia + max(self.uczestnicy[uczestnik1], self.uczestnicy[uczestnik2])
+            instrukcja = instrukcja_kopia.copy() if instrukcja_kopia is not None else [' '.join(przed_kopia) + ' |']
 
+            przed.remove(uczestnik1)
+            przed.remove(uczestnik2)
+            po.append(uczestnik1)
+            po.append(uczestnik2)
 
-    def start(self):
-        self.przed_kopia0 = list(self.uczestnicy.keys())
-        self.po_kopia0 = []
-        self.czas_kopia0 = 0
-        i = 0
-        wyniki = []
-        for i0 in self.wszystkie_mozliwosci_przed(self.przed_kopia0):
-            self.przed = self.przed_kopia0.copy()
-            self.po = self.po_kopia0.copy()
-            self.czas = self.czas_kopia0 + max(self.uczestnicy[i0[0]], self.uczestnicy[i0[1]])
-            self.przed.remove(i0[0])
-            self.przed.remove(i0[1])
-            self.po.append(i0[0])
-            self.po.append(i0[1])
-            self.przed_kopia1 = self.przed.copy()
-            self.po_kopia1 = self.po.copy()
-            self.czas_kopia1 = self.czas
-            for i1 in self.wszystkie_mozliwosci_po(self.po_kopia1):
-                self.przed = self.przed_kopia1.copy()
-                self.po = self.po_kopia1.copy()
-                self.czas = self.czas_kopia1 + self.uczestnicy[i1]
-                self.po.remove(i1)
-                self.przed.append(i1)
-                self.przed_kopia2 = self.przed.copy()
-                self.po_kopia2 = self.po.copy()
-                self.czas_kopia2 = self.czas
-                for i2 in self.wszystkie_mozliwosci_przed(self.przed_kopia2):
-                    self.przed = self.przed_kopia2.copy()
-                    self.po = self.po_kopia2.copy()
-                    self.czas = self.czas_kopia2 + max(self.uczestnicy[i2[0]], self.uczestnicy[i2[1]])
-                    self.przed.remove(i2[0])
-                    self.przed.remove(i2[1])
-                    self.po.append(i2[0])
-                    self.po.append(i2[1])
-                    self.przed_kopia3 = self.przed.copy()
-                    self.po_kopia3 = self.po.copy()
-                    self.czas_kopia3 = self.czas
-                    for i3 in self.wszystkie_mozliwosci_po(self.po_kopia3):
-                        self.przed = self.przed_kopia3.copy()
-                        self.po = self.po_kopia3.copy()
-                        self.czas = self.czas_kopia3 + self.uczestnicy[i3]
-                        self.po.remove(i3)
-                        self.przed.append(i3)
-                        self.przed_kopia4 = self.przed.copy()
-                        self.po_kopia4 = self.po.copy()
-                        self.czas_kopia4 = self.czas
-                        for i4 in self.wszystkie_mozliwosci_przed(self.przed_kopia4):
-                            self.przed = self.przed_kopia4.copy()
-                            self.po = self.po_kopia4.copy()
-                            self.czas = self.czas_kopia4 + max(self.uczestnicy[i4[0]], self.uczestnicy[i4[1]])
-                            self.przed.remove(i4[0])
-                            self.przed.remove(i4[1])
-                            self.po.append(i4[0])
-                            self.po.append(i4[1])
-                            i += 1
-                            wyniki.append([i0, i1, i2, i3, i4, self.czas])
+            self._dodaj_do_instrukcji(instrukcja, przed, po, czas)
 
-        min_czas = min(map(lambda i: i[5], wyniki))
-        for wynik in wyniki:
-            przed = list(self.uczestnicy.keys())
-            po = []
-            if wynik[5] == min_czas:
-                print('------------------')
-                print(f'CZAS: {min_czas}')
-                print(f"{', '.join(przed)} \t| {' '.join(po)}")
-                przed.remove(wynik[0][0])
-                przed.remove(wynik[0][1])
-                po.append(wynik[0][0])
-                po.append(wynik[0][1])
-                print(f"{', '.join(sorted(przed))} \t\t| {' '.join(sorted(po))}")
-                po.remove(wynik[1])
-                przed.append(wynik[1])
-                print(f"{', '.join(sorted(przed))} \t| {' '.join(sorted(po))}")
-                przed.remove(wynik[2][0])
-                przed.remove(wynik[2][1])
-                po.append(wynik[2][0])
-                po.append(wynik[2][1])
-                print(f"{', '.join(sorted(przed))} \t\t| {' '.join(sorted(po))}")
-                po.remove(wynik[3])
-                przed.append(wynik[3])
-                print(f"{', '.join(sorted(przed))} \t\t| {' '.join(sorted(po))}")
-                przed.remove(wynik[4][0])
-                przed.remove(wynik[4][1])
-                po.append(wynik[4][0])
-                po.append(wynik[4][1])
-                print(f"{', '.join(sorted(przed))} \t\t| {' '.join(sorted(po))}")
-                print('------------------')
+            if len(po) < len(self.uczestnicy):
+                self._przesun_do_przed(przed.copy(), po.copy(), czas, instrukcja.copy()) 
+            else:
+                instrukcja.append(czas)
+                self.instrukcje.append(instrukcja)
 
+    def _przesun_do_przed(self, przed_kopia: list, po_kopia: list, czas_kopia: int, instrukcja_kopia: list):
+        for uczestnik in po_kopia:
+            po = po_kopia.copy()
+            przed = przed_kopia.copy()
+            czas = czas_kopia + self.uczestnicy[uczestnik]
+            instrukcja = instrukcja_kopia.copy()
 
+            po.remove(uczestnik)
+            przed.append(uczestnik)
+
+            self._dodaj_do_instrukcji(instrukcja, przed, po, czas)
+
+            self._przesun_do_po(przed.copy(), po.copy(), czas, instrukcja.copy())
+
+    def oblicz(self):
+        self.start = time.time()
+        self._przesun_do_po(list(self.uczestnicy.keys()))
+        self.stop = time.time()
+
+    def wynik(self):
+        najkrotszy_czas = min(map(lambda i: i[-1], self.instrukcje))
+        ile_sposobow = sum(map(lambda i: i[-1] == najkrotszy_czas, self.instrukcje))
+
+        for uczestnik, czas in self.uczestnicy.items():
+            print(uczestnik, '=', czas, end='; ')
+
+        print(f'\nPrzeanalizowałem {len(self.instrukcje)} możliwości w {round((self.stop-self.start)*100, 2)}ms.')
+        print(f'Najszybciej uczestnicy przejdą w {najkrotszy_czas}min.')
+        print(f'Mogą to zrobić na {ile_sposobow} spos{"ób" if ile_sposobow == 1 else ("oby" if ile_sposobow < 5 else "obów")}.')
         
+        i = 0
+        for instrukcja in self.instrukcje:
+            if instrukcja[-1] == najkrotszy_czas:
+                i += 1
+                print(f'{i}.', '-'*int(5*len(self.uczestnicy)-len(str(i))))
+                print('\n'.join(instrukcja[:-1]))
 
-zadanie = PrzeprawaPrzezRzeczke()
-zadanie.start()
+
+zadanie = PrzeprawaPrzezMost()
+zadanie.wczytaj_dane()
+zadanie.oblicz()
+zadanie.wynik()
